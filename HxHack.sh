7a6a2b2df89340125e9619c63d737aead3bb4d24
Termux <70668507+TermuxHackz@users.noreply.github.com>
2020-09-04 16:31:10 +0100

    Add files via upload

---
+++ HxHack.sh
@@ -0,0 +1,12 @@
+echo -e "\e[34mHello Fellow Hacker
+Welcome to Termux-Android-Hacker Tool developed by your admin\e[0m";
+echo -e "\e[32mThis tool will enable you to Hack Facebook Accounts and All Social media accounts
+Type in \"Hack\" to continue...\e[0m";
+read -p 'Hack' userinput1;
+#
+if [ "${userinput1:-}" = "Hack" ]
+then
+	echo -e "\e[31mThis tool is Under development by the developer.....and will be ready soon enough
+Ha4ckers Beware
+We are legion\e[0m";
+fi
